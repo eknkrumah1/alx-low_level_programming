3b4e226e28a8accd36e2fb886bc3f472120779d5
eknkrumah1 <nkrumah.emmanuel8@gmail.com>
2024-07-03 20:19:36 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
