c034530380e744677277071c3705cc0e1af05075
eknkrumah1 <nkrumah.emmanuel8@gmail.com>
2024-07-06 06:41:07 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
- * Return: 0
+ * main - function to print combination of two nnumbers
+ *
+ * Return: 0 on success
  */
+
 int main(void)
 {
-	int c;
-	int d = 0;
+	int i = '0';
+	int j = '0';
 
-	while (d < 10)
+	while (i <= '9')
 	{
-		c = 0;
-		while (c < 10)
+		while (j <= '9')
 		{
-			if (d != c && d < c)
+			if (!(i > j) || i == j)
 			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-
-			c++;
+			j++;
 		}
-		d++;
+		j = '0';
+		i++;
 	}
-	putchar('\n');
-	return (0);
+return (0);
 }
